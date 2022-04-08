191023fa27aab6c7be97224f2d320a9e4ed8b9d1
AveryData <33168903+AveryData@users.noreply.github.com>
2022-04-08 11:11:04 -0600

    changed to cookies

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,35 +4,36 @@ import math
 import pandas as pd
 import streamlit as st
 
-"""
-# Welcome to Streamlit!
+import datetime
+st.write("# Cookie Manager")
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
+@st.cache(allow_output_mutation=True)
+def get_manager():
+    return stx.CookieManager()
 
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
+cookie_manager = get_manager()
 
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
+st.subheader("All Cookies:")
+cookies = cookie_manager.get_all()
+st.write(cookies)
 
+c1, c2, c3 = st.columns(3)
 
-with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
-
-    Point = namedtuple('Point', 'x y')
-    data = []
-
-    points_per_turn = total_points / num_turns
-
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
-
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
+with c1:
+    st.subheader("Get Cookie:")
+    cookie = st.text_input("Cookie", key="0")
+    clicked = st.button("Get")
+    if clicked:
+        value = cookie_manager.get(cookie=cookie)
+        st.write(value)
+with c2:
+    st.subheader("Set Cookie:")
+    cookie = st.text_input("Cookie", key="1")
+    val = st.text_input("Value")
+    if st.button("Add"):
+        cookie_manager.set(cookie, val, expires_at=datetime.datetime(year=2022, month=2, day=2))
+with c3:
+    st.subheader("Delete Cookie:")
+    cookie = st.text_input("Cookie", key="2")
+    if st.button("Delete"):
+        cookie_manager.delete(cookie)
