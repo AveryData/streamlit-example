76dea0746a5de570169fc6ea3162404ab816e9fc
AveryData <33168903+AveryData@users.noreply.github.com>
2022-04-08 11:28:05 -0600

    trying new cookies thing

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,16 @@ import math
 import pandas as pd
 import streamlit as st
 import extra_streamlit_components as stx
-import datetime
+from streamlit_cookies_manager import EncryptedCookieManager
+
+
+cookies = EncryptedCookieManager(
+    # This prefix will get added to all your cookie names.
+    # This way you can run your app on Streamlit Cloud without cookie name clashes with other apps.
+    prefix="ktosiek/streamlit-cookies-manager/",
+    # You should really setup a long COOKIES_PASSWORD secret if you're running on Streamlit Cloud.
+    password=os.environ.get("COOKIES_PASSWORD", "My secret password"),
+)
 
 import datetime
 st.write("# Cookie Manager")
@@ -34,6 +43,10 @@ with c2:
     val = st.text_input("Value")
     if st.button("Add"):
         cookie_manager.set(cookie, val, expires_at=datetime.datetime(year=2022, month=2, day=2))
+        cookies['a-cookie'] = value  # This will get saved on next rerun
+        cookies.save()
+        
+        
 with c3:
     st.subheader("Delete Cookie:")
     cookie = st.text_input("Cookie", key="2")
